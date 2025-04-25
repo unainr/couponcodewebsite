611435ffc34fc2b0dd62771fba529ff0f60efce0
UNAINR <munain605@gmail.com>
2025-04-25 23:20:22 +0500

    site

---
+++ next-sitemap.config.ts
@@ -0,0 +1,9 @@
+import type { IConfig } from 'next-sitemap';
+
+const config: IConfig = {
+  siteUrl: 'https://redeemlynow.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
+
+export default config;
