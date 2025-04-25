49f200496132171db8d6bf8bde63b50d8d95fa64
UNAINR <munain605@gmail.com>
2025-04-25 23:37:20 +0500

    fsite

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://redeemlynow.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
--- next-sitemap.config.ts
+++
@@ -1,9 +0,0 @@
-import type { IConfig } from 'next-sitemap';
-
-const config: IConfig = {
-  siteUrl: 'https://redeemlynow.com',
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-};
-
-export default config;
